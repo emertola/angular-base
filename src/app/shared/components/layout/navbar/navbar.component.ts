3b20fe1aa0d6ae3012222d927878c228120b04ae
emertola <emertola@gmail.com>
2021-09-30 23:54:43 +0800

    Angular Bootstrap Starter (initial)

---
+++ src/app/shared/components/layout/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  navbarOpen: boolean | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
